607ba1eb70be2cd75646d2efbabd1fa1faebc563
rabbs858 <dactyle@gmail.com>
2016-12-06 23:24:20 -0800

    API uploaded, connected, extremely barebones 11:24 PM

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -7,14 +7,27 @@ import yahoofinance.Stock;
 
 /**
  * Created by dacty on 12/6/2016.
+ *
+ * TODO: connecting fine it looks like, now we need to do something cool with the data. check out the readme @ lib/README.md -rob
  */
 public class Sandbox {
 
    public static void main(String[] args) throws IOException {
 
        Stock stock = YahooFinance.get("INTC");
+
+       BigDecimal price = stock.getQuote().getPrice();
+       BigDecimal change = stock.getQuote().getChangeInPercent();
+       BigDecimal peg = stock.getStats().getPeg();
+       BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+
+       stock.print();
+
+
+       /*Stock stock = YahooFinance.get("INTC");
        BigDecimal price = stock.getQuote(true).getPrice();
        System.out.println(price);
+       System.err.println("Hello, test.");*/
 
    }
 
