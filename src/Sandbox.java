16c9d2c7d670d89e13adcb41057b6a4c9f2f11e0
rabbs858 <dactyle@gmail.com>
2016-12-06 23:10:23 -0800

    API uploaded, connected, extremely barebones 11:10 PM

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,13 +1,22 @@
 import yahoofinance.YahooFinance;
-import yahoofinance.Stock;
+
+import java.io.IOException;
 import java.util.Map;
+import java.math.BigDecimal;
+import yahoofinance.Stock;
 
 /**
  * Created by dacty on 12/6/2016.
  */
 public class Sandbox {
 
-   Stock stock = new Stock("GOOG");
+   public static void main(String[] args) throws IOException {
 
-    
+       Stock stock = YahooFinance.get("INTC");
+       BigDecimal price = stock.getQuote(true).getPrice();
+       System.out.println(price);
+
+   }
+
+
 }
