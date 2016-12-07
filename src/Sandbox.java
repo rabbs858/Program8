9136dab0e3a00f7b3c7bc50441edb98d569c1b85
rabbs858 <dactyle@gmail.com>
2016-12-06 20:16:35 -0800

    API uploaded, not connted, 8:16 PM

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,5 +1,21 @@
+import yahoofinance.YahooFinance
+import yahoofinance.Stock;
+import yahoofinance.Utils;
+import java.math.BigDecimal;
+
 /**
  * Created by dacty on 12/6/2016.
  */
 public class Sandbox {
+
+    Stock stock = YahooFinance.get("INTC");
+
+    BigDecimal price = stock.getQuote().getPrice();
+    BigDecimal change = stock.getQuote().getChangeInPercent();
+    BigDecimal peg = stock.getStats().getPeg();
+    BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+
+    stock.print();
+
+
 }
