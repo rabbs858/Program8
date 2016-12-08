d3ace90d5e9072d0122c0566599724ad2346f5af
rabbs858 <dactyle@gmail.com>
2016-12-08 00:01:40 -0800

    Almost there 12:01 PM 12/8/2016

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,5 +1,6 @@
 import yahoofinance.YahooFinance;
 import yahoofinance.Stock;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -7,31 +8,39 @@ import java.util.Scanner;
 
 /**
  * Created by dacty on 12/6/2016.
- *
  */
 public class Sandbox {
 
-   public static void main(String[] args) throws IOException {
-       ArrayList<Stock> list = new ArrayList<>();
-       Scanner input = new Scanner(System.in);
-       System.out.println("Please enter up a list of stock symbols separated by spaces:");
-        while (input.hasNext()) {
-            list.add(addStock(input.next()));
+    public static void main(String[] args) throws IOException {
+        ArrayList<Stock> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter up a list of stock symbols separated by spaces:");
+
+        String userInput = input.nextLine();
+        String[] symbols = userInput.split(" ");
+
+        for (String stock : symbols) {
+            try {
+                list.add(addStock(stock));
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
         }
 
         //TODO: print the above list to a cool lookin chart
         list.toString();
-   }
+    }
 
-   public static Stock addStock(String s) throws IOException {
-       Stock stock = YahooFinance.get(s);
-       return stock;
-   }
+    public static Stock addStock(String s) throws IOException {
+        Stock stock = YahooFinance.get(s);
+        return stock;
+    }
 
-  public void printStocks(ArrayList<Stock> a) {
+    public void printStocks(ArrayList<Stock> a) {
 
-  }
-
+    }
 
 
 }
