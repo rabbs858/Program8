2fa3b4f59ef422b5eba08ee632bdd3f50ebf0c86
rabbs858 <dactyle@gmail.com>
2016-12-07 23:26:11 -0800

    Basics getting in order 11:26 PM 12/7/2016

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,7 +1,6 @@
 import yahoofinance.YahooFinance;
-
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
 import java.math.BigDecimal;
 import yahoofinance.Stock;
 
@@ -14,22 +13,22 @@ public class Sandbox {
 
    public static void main(String[] args) throws IOException {
 
-       Stock stock = YahooFinance.get("INTC");
+       ArrayList<Stock> list = new ArrayList<>();
 
+       list.add(addStock("TMO"));
+       System.out.println(list.toString());
+   }
+
+   public static Stock addStock(String s) throws IOException {
+       Stock stock = YahooFinance.get(s);
        BigDecimal price = stock.getQuote().getPrice();
        BigDecimal change = stock.getQuote().getChangeInPercent();
        BigDecimal peg = stock.getStats().getPeg();
        BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-
-       stock.print();
-
-
-       /*Stock stock = YahooFinance.get("INTC");
-       BigDecimal price = stock.getQuote(true).getPrice();
-       System.out.println(price);
-       System.err.println("Hello, test.");*/
-
+       return stock;
    }
+
+
 
 
 }
