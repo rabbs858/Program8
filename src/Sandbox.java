d62980d3b986a53c1d1b01c3de3d6475f07513b9
rabbs858 <dactyle@gmail.com>
2016-12-07 23:48:48 -0800

    Basics getting in order 11:48 PM 12/7/2016

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,33 +1,36 @@
 import yahoofinance.YahooFinance;
+import yahoofinance.Stock;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.math.BigDecimal;
-import yahoofinance.Stock;
+import java.util.Scanner;
+
 
 /**
  * Created by dacty on 12/6/2016.
  *
- * TODO: connecting fine it looks like, now we need to do something cool with the data. check out the readme @ lib/README.md -rob
  */
 public class Sandbox {
 
    public static void main(String[] args) throws IOException {
-
        ArrayList<Stock> list = new ArrayList<>();
+       Scanner input = new Scanner(System.in);
+       System.out.println("Please enter up a list of stock symbols separated by spaces:");
+        while (input.hasNext()) {
+            list.add(addStock(input.next()));
+        }
 
-       list.add(addStock("TMO"));
-       System.out.println(list.toString());
+        //TODO: print the above list to a cool lookin chart
+        list.toString();
    }
 
    public static Stock addStock(String s) throws IOException {
        Stock stock = YahooFinance.get(s);
-       BigDecimal price = stock.getQuote().getPrice();
-       BigDecimal change = stock.getQuote().getChangeInPercent();
-       BigDecimal peg = stock.getStats().getPeg();
-       BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
        return stock;
    }
 
+  public void printStocks(ArrayList<Stock> a) {
+
+  }
 
 
 
